331e145248ad9b25a581adf52b2a5e55b3505aa9
Rishav Jha <rishumour10@gmail.com>
2026-08-12 22:42:59 +0530

    0039-COMBINATION-SUM Stats: Time: 3 ms (52.2%), Space: 14 MB (94.1%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    void backtrack(const vector<int>& candidates, int target, int start, vector<int>& current, vector<vector<int>>& result) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+        for (int i = start; i < candidates.size(); ++i) {
+            current.push_back(candidates[i]);
+            backtrack(candidates, target - candidates[i], i, current, result);
+            current.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+       vector<vector<int>> result;
+        vector<int> current;
+        
+        backtrack(candidates, target, 0, current, result);
+        
+        return result;
+    }
+};
