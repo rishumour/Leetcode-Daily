3e729dfa0844796916259171ae9e53a048a42272
Rishav Jha <rishumour10@gmail.com>
2026-08-16 22:35:51 +0530

    2156-STONE-GAME-IX Stats: Time: 1 ms (82.74%), Space: 131.2 MB (69.29%) - LeetHub

---
+++ 2156-stone-game-ix/2156-stone-game-ix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        vector<int> counts(3, 0);
+        for (int stone : stones) {
+            counts[stone % 3]++;
+        }
+        if (counts[0] % 2 == 0) {
+            return counts[1] > 0 && counts[2] > 0;
+        } 
+        else {
+            return abs(counts[1] - counts[2]) > 2;
+        }
+    }
+};
